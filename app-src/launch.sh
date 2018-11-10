ae66678b668a38d886021230a4476fb86039a26a
Chris McCormick <chris@mccormick.cx>
2018-11-10 21:00:57 +0800

    Settings folder name.

--- app-src/launch.sh
+++ app-src/launch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-chromium-browser --user-data-dir=./chrome --window-size=600,600 --allow-file-access-from-files --app=http://localhost:3449/
+chromium-browser --user-data-dir=./chrome-svg-animation-assistant --window-size=600,600 --allow-file-access-from-files --app=http://localhost:3449/
