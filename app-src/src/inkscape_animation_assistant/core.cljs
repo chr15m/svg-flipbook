54eb12509e2e8671d2f1f1b52fd650701eee22c9
Chris McCormick <chris@mccormick.cx>
2018-11-11 11:24:32 +0800

    Basic animation functionality now working.

--- app-src/src/inkscape_animation_assistant/core.cljs
+++ app-src/src/inkscape_animation_assistant/core.cljs
@@ -2,6 +2,12 @@
     (:require
       [reagent.core :as r]))
 
+(def initial-state
+  {:playing false
+   :svg nil
+   :last nil
+   :file nil})
+
 (defn read-file [file cb]
   (let [reader (js/FileReader.)]
     (aset reader "onload" #(cb (.. % -target -result)))
@@ -17,13 +23,62 @@
       (read-file file (fn [content]
                         (swap! state assoc :svg content))))))
 
-(defn file-selected [state ev]
+(defn file-selected! [state ev]
   (let [input (.. ev -target)
         file (and (.-files input) (aget (.-files input) 0))]
     (read-file file (fn [content]
                       (when (= (.indexOf content "viewBox") -1)
                         (js/alert "Warning: SVG has no viewBox.\nScale will be fixed at original size."))
                       (swap! state assoc :file file :last (.-lastModifiedDate file) :svg content)))))
+
+(defn layer-get-delay [layer]
+  (let [default 100]
+    (if layer
+      (let [label (.getAttribute layer "inkscape:label")
+            delayparameter (.match label #"\((\d+)\)")]
+        (if delayparameter (js/parseInt (aget delayparameter 1)) default))
+      default)))
+
+(defn layer-is-static [layer]
+  (when layer
+    (-> layer
+        (.getAttribute "inkscape:label")
+        (.indexOf "tatic")
+        (not= -1))))
+
+(defn layers-get-all []
+  (js/Array.from (.querySelectorAll js/document "[inkscape\\:groupmode='layer']")))
+
+(defn flip-layers [cb layers]
+  (doall
+    (map-indexed
+      (fn [i l]
+        (aset (.-style l) "display"
+              (if (cb i l)
+                ""
+                "none")))
+      layers)))
+
+(defn animate! [state & [frame]]
+  (let [layers (layers-get-all)
+        length (count layers)
+        current-frame (mod (or frame 0) length)
+        layer (aget layers (or current-frame 0))
+        frame-time (layer-get-delay layer)
+        static (layer-is-static layer)]
+    (when (@state :playing)
+      (when (or (not static) (not frame))
+        (flip-layers (fn [i l] (or (= i current-frame) (layer-is-static l))) layers))
+      (js/setTimeout (partial animate! state (+ frame 1)) frame-time))))
+
+(defn play! [state ev]
+  (let [timer (js/setTimeout (partial animate! state) 1)]
+    (swap! state #(-> %
+                      (assoc :timer timer)
+                      (update-in [:playing] not)))))
+
+(defn pause! [state ev]
+  (swap! state update-in [:playing] not))
 
 ;; -------------------------
 ;; Views
@@ -34,30 +89,39 @@
     [:path {:fill "#888" :d "m1781,605q0,35-53,35h-1088q-40,0-85.5-21.5t-71.5-52.5l-294-363q-18-24-18-40,0-35,53-35h1088q40,0,86,22t71,53l294,363q18,22,18,39zm-1141,163h768v160q0,40-28,68t-68,28h-576q-40,0-68,28t-28,68v64q0,40-28,68t-68,28h-320q-40,0-68-28t-28-68v-853l256,315q44,53,116,87.5t140,34.5zm1269-163q0-62-46-120l-295-363q-43-53-116-87.5t-140-34.5h-1088q-92,0-158,66t-66,158v960q0,92,66,158t158,66h320q92,0,158-66t66-158v-32h544q92,0,158-66t66-158v-160h192q54,0,99-24.5t67-70.5q15-32,15-68z"}]]])
 
 (defn component-play-pause [state]
-  [:svg#play-pause {:width 64 :height 64 :viewBox "0 0 1792 1792"
-         :on-click #(swap! state update-in [:playing] not)}
+  [:svg#play-pause.icon {:viewBox "0 0 1792 1792"
+         :on-click (partial (if (@state :playing) pause! play!) state)}
    [:path {:d
            (if (@state :playing)
-             "M832 1184v-576q0-14-9-23t-23-9h-256q-14 0-23 9t-9 23v576q0 14 9 23t23 9h256q14 0 23-9t9-23zm448 0v-576q0-14-9-23t-23-9h-256q-14 0-23 9t-9 23v576q0 14 9 23t23 9h256q14 0 23-9t9-23zm384-288q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"
+             "M1216 1184v-576q0-14-9-23t-23-9h-576q-14 0-23 9t-9 23v576q0 14 9 23t23 9h576q14 0 23-9t9-23zm448-288q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"
              "M896 128q209 0 385.5 103t279.5 279.5 103 385.5-103 385.5-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103zm384 823q32-18 32-55t-32-55l-544-320q-31-19-64-1-32 19-32 56v640q0 37 32 56 16 8 32 8 17 0 32-9z")}]])
+
+(defn component-close [state]
+  [:svg#close.icon {:viewBox "0 0 1792 1792"
+                    :on-click #(reset! state initial-state)}
+   [:path {:d "M1277 1122q0-26-19-45l-181-181 181-181q19-19 19-45 0-27-19-46l-90-90q-19-19-46-19-26 0-45 19l-181 181-181-181q-19-19-45-19-27 0-46 19l-90 90q-19 19-19 46 0 26 19 45l181 181-181 181q-19 19-19 45 0 27 19 46l90 90q19 19 46 19 26 0 45-19l181-181 181 181q19 19 45 19 27 0 46-19l90-90q19-19 19-46zm387-226q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"}]])
 
 (defn component-app [state]
   (if (@state :file)
     [:div#container
-     [:div#animation {:dangerouslySetInnerHTML {:__html (@state :svg)}}]
+     [:div#animation {:dangerouslySetInnerHTML {:__html (@state :svg)}
+                      :ref #(when % (flip-layers (fn [i l] (or (= i 0))) (layers-get-all)))}]
      [:div#interface
+      [component-close state]
       [component-play-pause state]]]
     [:div#choosefile
      [:div
       [:label 
-       [:input#fileinput {:type "file" :accept ".svg" :on-change (partial file-selected state)}]
+       [:input#fileinput {:type "file"
+                          :accept ".svg"
+                          :on-change (partial file-selected! state)}]
        [component-folder-icon]
        [:p "Choose SVG"]]]]))
 
 ;; -------------------------
 ;; Initialize app
 
-(defonce state (r/atom {}))
+(defonce state (r/atom initial-state))
 
 (defn mount-root []
   (r/render [component-app state] (.getElementById js/document "app")))
