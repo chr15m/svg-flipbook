05c604c8ef2aa0b908a4b1af09d7e2e8ee7310e6
Chris McCormick <chris@mccormick.cx>
2019-09-10 21:43:59 +0800

    Add id to export button.

--- app-src/src/inkscape_animation_assistant/core.cljs
+++ app-src/src/inkscape_animation_assistant/core.cljs
@@ -82,7 +82,7 @@
              "M896 128q209 0 385.5 103t279.5 279.5 103 385.5-103 385.5-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103zm384 823q32-18 32-55t-32-55l-544-320q-31-19-64-1-32 19-32 56v640q0 37 32 56 16 8 32 8 17 0 32-9z")}]])
 
 (defn component-export [state animation-script]
-  [:a {:href (make-export-url state animation-script) :download (.replace (.-name (@state :file)) ".svg" "-animated.svg")}
+  [:a {:href (make-export-url state animation-script) :download (.replace (.-name (@state :file)) ".svg" "-animated.svg") :id "exportbtn"}
    [:svg#export.icon {:viewBox "0 0 1792 1792"}
     [:path {:d "M1344 1344q0-26-19-45t-45-19-45 19-19 45 19 45 45 19 45-19 19-45zm256 0q0-26-19-45t-45-19-45 19-19 45 19 45 45 19 45-19 19-45zm128-224v320q0 40-28 68t-68 28h-1472q-40 0-68-28t-28-68v-320q0-40 28-68t68-28h465l135 136q58 56 136 56t136-56l136-136h464q40 0 68 28t28 68zm-325-569q17 41-14 70l-448 448q-18 19-45 19t-45-19l-448-448q-31-29-14-70 17-39 59-39h256v-448q0-26 19-45t45-19h256q26 0 45 19t19 45v448h256q42 0 59 39z"}]]])
 
